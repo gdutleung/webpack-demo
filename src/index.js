238855745454d84d614afbab2b57e67ffa750c15
lleung <915518319@qq.com>
2019-01-24 16:23:40 +0800

    输出管理

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import _ from 'lodash'
 import './style.css'
 import Img from './img/下载.jpg'
+import printMe from './print'
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button')
 	
 	// Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello')
 	
-	var myImg = new Image()
-	myImg.src = Img
+	btn.innerHTML = 'click me'
+	btn.onclick = printMe
 	
-	element.appendChild(myImg)
+	element.appendChild(btn)
 	
 	return element;
 }
